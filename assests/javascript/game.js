e0dd5ad3c29ef3862ea3a146685147b4d608c04a
Kaiden Munsey <kaidenmnsey@aol.com>
2020-04-15 16:19:29 -0400

    <adding new images, deleting old and adding html/css>

---
+++ assests/javascript/game.js
@@ -0,0 +1,15 @@
+$(document).ready(function) {
+
+let teamNames= ["jets", "packers", "patriots", "rams", "eagles", "broncos", "bears", "steelers", "red skins", "dolphins", "ravens", "raiders", "seahawks", "saints", "falcons", "titans", "buccaneers"]
+
+const maxGuesses = 10
+
+let letterGuessed = ['']
+let 
+
+
+
+
+
+
+}
