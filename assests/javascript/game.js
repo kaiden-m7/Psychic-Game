9753b96a17fbf3656cd9379b5cd8d7a763e81b54
Kaiden Munsey <kaidenmnsey@aol.com>
2020-04-15 17:26:22 -0400

    <adding variables in js and starting functions>

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,11 +1,32 @@
 $(document).ready(function) {
+    let letterChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-let teamNames= ["jets", "packers", "patriots", "rams", "eagles", "broncos", "bears", "steelers", "red skins", "dolphins", "ravens", "raiders", "seahawks", "saints", "falcons", "titans", "buccaneers"]
+    let wins = 0;
+    let losses = 0;
+    let numGuesses = 10;
+    let userAnswers = [];
 
-const maxGuesses = 10
+    const letterToBeGuessed = computerOptions[Math.floor(Math.random() * computerOptions.length)];
 
-let letterGuessed = ['']
-let 
+    document.onkeydown = function(event) {
+        
+    }
+
+//avoid answering the same letter twice
+    let isDuplicate = false;
+for (let i = 0; i < userAnswers.length; i++) { //for every previous answer
+    if (currentAnswer == userAnswers[i]) { //check if the current answer is a previous one
+        isDuplicate = true;
+        break; //exit loop
+    }
+}
+if (!isDuplicate) { //different than previous
+    userAnswers[userAnswers.length] = currentAnswer;
+    
+}
+
+
+
 
 
 
