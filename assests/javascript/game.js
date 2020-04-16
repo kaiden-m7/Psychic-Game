7d6b0af472b234355704dc37745cd19f62b237b6
Kaiden Munsey <kaidenmnsey@aol.com>
2020-04-16 15:33:13 -0400

    <added JS code to randmon letter choice by computer>

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,19 +1,44 @@
 $(document).ready(function) {
-    let letterChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    let letterChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-    let wins = 0;
-    let losses = 0;
-    let numGuesses = 10;
-    let userAnswers = [];
+    let winsCounter = 0; //tracks wins
+    let lossesCounter = 0; //tracks loses
+    let numGuesses = 10; //numbers of guesses left
+    let userAnswers = []; //letters chosen by player
+    let counter = 0; //counting numbers of wins/loses
 
-    const letterToBeGuessed = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-    let userGuess = event.key
-    document.onkeydown = function(event) {
-        if (userGuess === letterToBeGuessed)
-    }
+    
+    const computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)]; //computer randmonly icking a letter
+    
+    let userGuess = event.key //indicating user is using letters on keyboard
 
-//avoid answering the same letter twice
-    let isDuplicate = false;
+   
+   
+    document.onkeyup = function(event) { //once key is 'pressed' it becomes players answer
+    numGuesses--;
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+  guessedLetters.push(userGuess);
+  updateGuessesLeft();
+  updateGuessesSoFar();
+
+        if (guessesLeft > 0){ //if users guess matches computer guess 
+            if (userGuess == letterToGuess){
+                wins++;
+                document.getElementById('#totalWins').innerHTML = "Wins: " + wins;
+    
+            }
+        }else if(guessesLeft == 0){ //if users guess does not match computer guess
+            losses++;
+            document.getElementById('#losses').innerHTML = "Losses: " + losses;
+        }
+};
+
+var updateGuessesSoFar = function() { //showing users answers and seperateing by a comma with a space
+    document.getElementById('#guessedLetters').innerHTML = "What you have guessed so far: " + guessedLetters.join(', ');
+  };
+
+    let isDuplicate = false; //avoid answering the same letter twice
 for (let i = 0; i < userAnswers.length; i++) { //for every previous answer
     if (currentAnswer == userAnswers[i]) { //check if the current answer is a previous one
         isDuplicate = true;
