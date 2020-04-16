1cff1e0fe7dc96431c555574041979c8eec4333e
Kaiden Munsey <kaidenmnsey@aol.com>
2020-04-16 15:51:37 -0400

    <adding counter and functions for change win and lose score>

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -14,13 +14,13 @@ $(document).ready(function) {
 
    
    
-    document.onkeyup = function(event) { //once key is 'pressed' it becomes players answer
+    document.onkeydown = function(event) { //once key is 'pressed' it becomes players answer
     numGuesses--;
-  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-  guessedLetters.push(userGuess);
-  updateGuessesLeft();
-  updateGuessesSoFar();
+    userAnswers.push(userGuess);
+        updateGuessesLeft();
+        updateGuessesSoFar();
 
         if (guessesLeft > 0){ //if users guess matches computer guess 
             if (userGuess == letterToGuess){
@@ -33,18 +33,20 @@ $(document).ready(function) {
             document.getElementById('#losses').innerHTML = "Losses: " + losses;
         }
 };
-
-var updateGuessesSoFar = function() { //showing users answers and seperateing by a comma with a space
-    document.getElementById('#guessedLetters').innerHTML = "What you have guessed so far: " + guessedLetters.join(', ');
+    let updateGuessesLeft = function() { //getting userAnswers from user 
+    document.getElementById('#remainingGuesses').innerHTML = "Guesses Remaining: " + numGuesses;
+  };
+    let updateGuessesSoFar = function() { //showing users answers and seperateing by a comma with a space
+    document.getElementById('#guessedLetters').innerHTML = "What you have guessed so far: " + userAnswers.join(', ');
   };
 
     let isDuplicate = false; //avoid answering the same letter twice
-for (let i = 0; i < userAnswers.length; i++) { //for every previous answer
+    for (let i = 0; i < userAnswers.length; i++) { //for every previous answer
     if (currentAnswer == userAnswers[i]) { //check if the current answer is a previous one
         isDuplicate = true;
         break; //exit loop
     }
-}
+    }
     if (!isDuplicate) { //different than previous
     userAnswers[userAnswers.length] = currentAnswer;
     
