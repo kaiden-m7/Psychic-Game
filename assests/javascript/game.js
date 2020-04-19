9755f565d345503d30ae5f0d918cac6fcc415728
Kaiden Munsey <kaidenmnsey@aol.com>
2020-04-18 21:47:11 -0400

    <adding a reset function>

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,4 +1,3 @@
-$document.ready(function(){ 
 
     let letterChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
@@ -8,61 +7,55 @@ $document.ready(function(){
     let userAnswers = []; //letters chosen by player
     let letterPressed = false; //did the player press a key
     let isDuplicate = false; //did player already choose that letter
+    let letToGuessing = null;
+    let computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
     
-
-
-        function isLetter(event.keypress) { //allow player to start on keypress 
-            let userAnswers = [];
-            letterPressed = true;
-            document.getElementById("#totalWins").innerHTML = 'Wins: ';
-            document.getElementById("#losses").innerHTML = 'Losses: ';
-            let computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)]; //computer randmonly picking a letter
-            console.log("Select any letter key to play.");
-        
-            document.onkeydown = function(event) {
-            var userGuess = event.key;
-
-            if (letterChoices.indexOf(userGuess) > -1){ 
-
-                    if (guessesLeft !== 0){ //if letter chosen is not computers guess 
-                    if ((userGuess !== computerGuess) && (numGuesses > 0)){
-			    	userAnswers.push(userGuess);
-                    numGuesses--;
-                    lossesCounter++;
-			    	document.getElementById("remainingGuesses").innerHTML = "Guesses Remaining: " + guessesLeft;
-                    document.getElementById("guessedLetters").innerHTML = "What you have guessed so far: " + lettersGuessed.join(', ');
-                    
-                    } else if (userGuess === computerGuess){ //if letter chosen is computer guess
-			    	winsCounter ++;
-			    	numGuesses = 10;
-			    	document.getElementById("wins").innerHTML = "Wins: " + winCounter;
-			    	document.getElementById("remainingGuess").innerHTML = "Guesses Left : 10";
-                    document.getElementById("guessedLetters").innerHTML = "Your Guesses so far: ";
-                    reSet ()
-			        }
-
-                
-                    for (let i = 0; i < userAnswers.length; i++) { //for every previous answer
-                    if (currentAnswer == userAnswers[i]) { //check if the current answer is a previous one
-                    isDuplicate = true;
-                    alert("Try another letter you haven'/t guessed");
-                    break; //exit loop
-                        }
-                    }
-                }
-            }   
-
+    let newGuess = function() {
+    document.getElementById('#remainingGuesses').innerHTML = "Guesses left: " + guessesLeft;
+    };
+    
+    let pickNewLetter = function() {
+    computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
+    };
+    
+    let soFar = function() {
+    document.getElementById('#guessedLetters').innerHTML = "What you he guessed so far: " + userAnswers.join(', ');
+    };
+    
+    let reSet = function() {
+    numGuesses = 9;
+    userAnswes = [];
+    pickNewLetter();
+    newGuess();
+    soFar();
     }
-}
 
-     function reSet () {
-    numGuesses = 10;
-    userAnswers = [];
-    computerGuess =  letterChoices[Math.floor(Math.random() * letterChoices.length)];
-    }   
+    pickNewLetter();
+    newGuess();
+    document.onkeydown = function(event) {
+    numGuesses--;
+    let whatAreYouGuessing = String.fromCharCode(event.keyCode).toLowerCase();
+    userAnswers.push(whatAreYouGuessing);
+    newGuess();
+    soFar();
+    if (numGuesses > 0){
+    if (whatAreYouGuessing == computerGuess){
+    winnings++;
+    document.getElementById('#totalWins').innerHTML = "Wins: " + winsCounter;
+    reSet();
+    }
+    } else if(guessesLeft == 0){
+    losed++;
+    document.getElementById('#losses').innerHTML = "Losses: " + lossesCounter;
+    alert("Sorry! Click another letter to play again!");
+    reSet();
+    } else (ifDulplicate === true){
+        for (let i = 0; i < userAnswers.length; i++) { //for every previous answer
+            if ( == userAnswers[i]) { //check if the current answer is a previous one
+            isDuplicate = true;
+            alert("Try another letter you haven'/t guessed");
+            break; //exit loop
+    }
 
-});
 
-
-
-
+    };
